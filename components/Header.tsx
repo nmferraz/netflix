d6a3256cf431e17291f5913d5fac9fa0162240a6
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-18 19:48:19 +0100

    basic navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ function Header() {
           <li className="headerLink">My List</li>
         </ul>
       </div>
-      <div>
+      <div className="flex items-center space-x-4 text-sm font-light">
         <SearchIcon className="hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
