aac844c25380bf519b9108306693d0056d7c4fda
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-22 19:24:21 +0100

    mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
+import BasicMenu from './BasicMenu'
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -32,6 +33,9 @@ function Header() {
           height={100}
           className="cursor-pointer object-contain"
         />
+
+        <BasicMenu />
+
         <ul className="hidden space-x-4 md:flex">
           <li className="headerLink cursor-default font-semibold text-white hover:text-white">
             <Link href="/">Home</Link>
