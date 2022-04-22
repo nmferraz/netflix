5425383ecb181d3025043e7966dfbe20bb7e6369
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-22 15:56:25 +0100

    open modal from thumbnail

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image'
 import { Movie } from '../typings'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom.'
+import { DocumentData } from 'firebase/firestore'
 
 interface Props {
-  //movie: Movie || DocumentData  to firebase
+  //movie: Movie || DocumentData
   movie: Movie
 }
 
 function Thumbnail({ movie }: Props) {
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState)
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+    <div
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
