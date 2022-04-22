53f83089197cf494a7202f88b8439007ed9b1715
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-22 11:11:12 +0100

    login page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['image.tmdb.org'],
+    domains: ['rb.gy', 'image.tmdb.org'],
   },
   reactStrictMode: true,
 }
