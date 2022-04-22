be3795ab445f8f5bd46546274ac81401db3e0b1c
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-22 15:24:39 +0100

    loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Row from '../components/Row'
 import { Movie } from '../typing'
 import requests from '../utils/requests'
+import useAuth from '../hooks/useAuth'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -26,8 +27,10 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
+  const { loading } = useAuth()
+  if (loading === null) return null
   return (
-    <div className="bg-gradient-to-b relative h-screen from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home | Netflix | By nmferraz</title>
         <link rel="icon" href="/favicon.ico" />
